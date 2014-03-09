0b0e0b790ca4c1427fb00190dc7bff0c5a95b86d
Mathurin Petit <mpetit@actualys.com>
2014-03-09 21:48:15 +0100

    Commit project

---
+++ src/Omaracuja/UserBundle/Controller/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Omaracuja\UserBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Omaracuja\UserBundle\Entity\User;
+
+/**
+ * User controller.
+ *
+ * @Route("/user")
+ */
+class UserController extends Controller
+{
+
+    /**
+     * Lists all User entities.
+     *
+     * @Route("/", name="user")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('OmaracujaUserBundle:User')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a User entity.
+     *
+     * @Route("/{id}", name="user_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('OmaracujaUserBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
+    }
+}
---
+++ src/Omaracuja/UserBundle/OmaracujaUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Omaracuja\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OmaracujaUserBundle extends Bundle
+{
+}
