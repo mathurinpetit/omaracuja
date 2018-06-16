2edae3c73f9b1f524df57aa2f5ebcc705b1acfba
Mathurin Petit <mathurin.petit@gmail.com>
2018-06-16 16:16:05 +0200

    maj site marak

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,9 +20,17 @@ class DefaultController extends Controller
                 $dates[$line_num] = str_getcsv($date,';');
             }
         }
+        $string_onyetait = file('../onyetait.csv');
+        $onyetait = array();
+        foreach ($string_onyetait as $line_num => $onyetait_line) {
+            if($onyetait_line){
+                $onyetait[$line_num] = str_getcsv($onyetait_line,';');
+            }
+        }
+        //var_dump($onyetait); exit;
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'dates' => $dates
+            'dates' => $dates,'onyetait' => $onyetait
         ]);
     }
 }
